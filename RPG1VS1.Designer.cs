68a3963f4fdb4bf40ec37b080d0c644a31d5735f
Joaquin <joaquinmolina9306@gmail.com>
2025-02-02 16:19:57 -0300

    Primera subida

---
+++ RPG1VS1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace ProyectoFinal2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblPlayerHP = new System.Windows.Forms.Label();
+            this.lblEnemyHP = new System.Windows.Forms.Label();
+            this.btnAttack = new System.Windows.Forms.Button();
+            this.btnDefend = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPlayerHP
+            // 
+            this.lblPlayerHP.AutoSize = true;
+            this.lblPlayerHP.Location = new System.Drawing.Point(151, 255);
+            this.lblPlayerHP.Name = "lblPlayerHP";
+            this.lblPlayerHP.Size = new System.Drawing.Size(22, 13);
+            this.lblPlayerHP.TabIndex = 0;
+            this.lblPlayerHP.Text = "HP";
+            // 
+            // lblEnemyHP
+            // 
+            this.lblEnemyHP.AutoSize = true;
+            this.lblEnemyHP.Location = new System.Drawing.Point(605, 49);
+            this.lblEnemyHP.Name = "lblEnemyHP";
+            this.lblEnemyHP.Size = new System.Drawing.Size(22, 13);
+            this.lblEnemyHP.TabIndex = 1;
+            this.lblEnemyHP.Text = "HP";
+            // 
+            // btnAttack
+            // 
+            this.btnAttack.Location = new System.Drawing.Point(313, 455);
+            this.btnAttack.Name = "btnAttack";
+            this.btnAttack.Size = new System.Drawing.Size(75, 23);
+            this.btnAttack.TabIndex = 2;
+            this.btnAttack.Text = "Atacar";
+            this.btnAttack.UseVisualStyleBackColor = true;
+            this.btnAttack.Click += new System.EventHandler(this.btnAttack_Click);
+            // 
+            // btnDefend
+            // 
+            this.btnDefend.Location = new System.Drawing.Point(477, 455);
+            this.btnDefend.Name = "btnDefend";
+            this.btnDefend.Size = new System.Drawing.Size(75, 23);
+            this.btnDefend.TabIndex = 3;
+            this.btnDefend.Text = "Defenderse";
+            this.btnDefend.UseVisualStyleBackColor = true;
+            this.btnDefend.Click += new System.EventHandler(this.btnDefend_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ProyectoFinal2.Properties.Resources.arthur_lobato_muma_final__2_;
+            this.pictureBox2.Location = new System.Drawing.Point(608, 79);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(122, 123);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProyectoFinal2.Properties.Resources.arthur_lobato_kapi21;
+            this.pictureBox1.Location = new System.Drawing.Point(154, 281);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 125);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 549);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDefend);
+            this.Controls.Add(this.btnAttack);
+            this.Controls.Add(this.lblEnemyHP);
+            this.Controls.Add(this.lblPlayerHP);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RPG Carpinchos";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPlayerHP;
+        private System.Windows.Forms.Label lblEnemyHP;
+        private System.Windows.Forms.Button btnAttack;
+        private System.Windows.Forms.Button btnDefend;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ RPG1VS1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal2
+{
+    public partial class Form1 : Form
+    {
+
+        private int playerHP = 100; //ACÁ LO QUE HAGO ES GUARDAR LA VIDA DEL PERSONAJE Y ENEMIGO EN UNA VARIABLE.
+        private int enemyHP = 100;
+        private Random random = new Random(); //ACÁ CREO UN NÚMERO ALEATORIO PARA QUE LOS ATAQUE NO SIEMPRE HAGAN EL MISMO DAÑO.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAttack_Click(object sender, EventArgs e)
+        {
+            int playerDamage = random.Next(10, 20); //GENERO UN DAÑO ALEATORIO ENTRE 10 Y 20.
+            enemyHP -= playerDamage; //ACÁ SE DESCUENTA EL DAÑO GENERADO POR EL PERSONAJE AL ENEMIGO.
+            lblEnemyHP.Text = $"Enemigo HP: {enemyHP}"; //ACÁ DICE CUANTO ES EL DAÑO EJERCIDO.
+
+            if (enemyHP <= 0)
+            { MessageBox.Show("El enemigo ha sido derrotado.");
+                ResetGame();
+                return;
+            }
+            EnemyTurn();
+        }
+
+        private void btnDefend_Click(object sender, EventArgs e)
+        {
+            int reducedDamage = random.Next(5, 10); //ACÁ GENERO LA DEFENSA ENTRE 5 Y 10.
+            playerHP -= reducedDamage;
+            lblPlayerHP.Text = $"Jugador HP: {playerHP}";
+
+            if (playerHP <= 0)
+            {
+                MessageBox.Show("Has sido derrotado...");
+                ResetGame();
+
+            }
+        }
+        private void EnemyTurn()
+        {
+            int enemyDamage = random.Next(10, 15);
+            playerHP -= enemyDamage;
+            lblPlayerHP.Text = $"Jugador HP: {playerHP}";
+
+            if (playerHP <= 0)
+            {
+                MessageBox.Show("Has sido derrotado...");
+                ResetGame();
+            }
+        }
+        private void ResetGame()
+        { playerHP = 100;
+            enemyHP = 100;
+            lblPlayerHP.Text = "Jugador HP: 100";
+            lblEnemyHP.Text = " Enemigo HP: 100";
+        }
+    }
+}
